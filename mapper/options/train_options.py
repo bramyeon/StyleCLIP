5eceab52539e8c5925dcb73478f7084037635516
Bryan Nathanael Wijaya <bramyeon@gmail.com>
2023-12-03 02:18:23 +0900

    Update mapper loss

--- mapper/options/train_options.py
+++ mapper/options/train_options.py
@@ -30,7 +30,8 @@ class TrainOptions:
 		self.parser.add_argument('--id_lambda', default=0.1, type=float, help='ID loss multiplier factor')
 		self.parser.add_argument('--clip_lambda', default=1.0, type=float, help='CLIP loss multiplier factor')
 		self.parser.add_argument('--latent_l2_lambda', default=0.8, type=float, help='Latent L2 loss multiplier factor')
-
+		self.parser.add_argument('--mse_lambda', default=0.5, type=float, help='Pixel MSE loss multiplier factor')
+        
 		self.parser.add_argument('--stylegan_weights', default='../pretrained_models/stylegan2-ffhq-config-f.pt', type=str, help='Path to StyleGAN model weights')
 		self.parser.add_argument('--stylegan_size', default=1024, type=int)
 		self.parser.add_argument('--ir_se50_weights', default='../pretrained_models/model_ir_se50.pth', type=str, help="Path to facial recognition network used in ID loss")
--- mapper/training/coach.py
+++ mapper/training/coach.py
@@ -35,6 +35,8 @@ class Coach:
 			self.clip_loss = clip_loss.CLIPLoss(opts)
 		if self.opts.latent_l2_lambda > 0:
 			self.latent_l2_loss = nn.MSELoss().to(self.device).eval()
+		if self.opts.mse_lambda > 0:
+			self.mse_loss = nn.MSELoss().to(self.device).eval()
 
 		# Initialize optimizer
 		self.optimizer = self.configure_optimizers()
@@ -241,6 +243,11 @@ class Coach:
 				loss_l2_latent = self.latent_l2_loss(w_hat, w)
 			loss_dict['loss_l2_latent'] = float(loss_l2_latent)
 			loss += loss_l2_latent * self.opts.latent_l2_lambda
+		if self.opts.mse_lambda > 0:
+			loss_mse = self.mse_loss(x_hat, x)
+			loss_dict['loss_mse'] = float(loss_mse)
+			loss += loss_mse * self.opts.mse_lambda
+            
 		loss_dict['loss'] = float(loss)
 		return loss, loss_dict
 
